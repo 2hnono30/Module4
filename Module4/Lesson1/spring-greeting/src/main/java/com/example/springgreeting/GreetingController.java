e075359e66fa41796b998d3ef9d84c887f929ce8
Quang Huy <2hnono30@gmail.com>
2022-09-30 09:02:53 +0700

    module 4 lesson 1 thuc hanh 2

--- Module4/Lesson1/spring-greeting/src/main/java/com/example/springgreeting/GreetingController.java
+++ Module4/Lesson1/spring-greeting/src/main/java/com/example/springgreeting/GreetingController.java
@@ -5,12 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-    public class GreetingController {
-        @GetMapping("/greeting")
-        public String greeting(@RequestParam String name, Model model) {
-            model.addAttribute("name", name);
-            return "index";
-        }
 
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
 }

978faffb2100667867b42f4e33599a59dff2808d
Quang Huy <2hnono30@gmail.com>
2022-10-14 09:24:13 +0700

    lesson13 baitap1

---
+++ Module4/lesson13/baitap1/src/main/java/common/Alert.java
@@ -0,0 +1,6 @@
+package common;
+public class Alert {
+    public static final int CREATE = 1;
+    public static final int EDIT = 2;
+    public static final int DELETE = 3;
+}
---
+++ Module4/lesson13/baitap1/src/main/java/service/ProvinceService.java
@@ -0,0 +1,14 @@
+package service;
+
+
+import model.ProvinceEntity;
+
+public interface ProvinceService {
+    public Iterable<ProvinceEntity> findAll();
+
+    public ProvinceEntity findById(Integer id);
+
+    public void save(ProvinceEntity province);
+
+    public void  remove(Integer id);
+}

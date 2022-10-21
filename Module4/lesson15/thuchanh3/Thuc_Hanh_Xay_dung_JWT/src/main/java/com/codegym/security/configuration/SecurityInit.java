457585f4c3fbb8ec2813ed918f5fb8e84932636c
Quang Huy <2hnono30@gmail.com>
2022-10-21 11:27:45 +0700

    thuchanh3 lesson 15 module4

---
+++ Module4/lesson15/thuchanh3/Thuc_Hanh_Xay_dung_JWT/src/main/java/com/codegym/security/configuration/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.codegym.security.configuration;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
